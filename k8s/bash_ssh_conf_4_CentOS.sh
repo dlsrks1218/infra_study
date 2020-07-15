d0f0076abc81d3d22050de9d4b8068f8194fbfae
jonghyunlim <dlsrks1218@daum.net>
2020-07-15 18:19:18 +0900

    centos에 ssh 붙었을때 키바인딩 해결, mysql컨테이너 실행시 -e 필요 이슈 해결

--- k8s/bash_ssh_conf_4_CentOS.sh
+++ k8s/bash_ssh_conf_4_CentOS.sh
@@ -4,3 +4,7 @@ now=$(date +"%m_%d_%Y")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+echo "bind '\"\\e\\e[C\": forward-word'" >> ~/.bash_profile
+echo "bind '\"\\e\\e[D\": backward-word'" >> ~/.bash_profile
+source ~/.bash_profile
